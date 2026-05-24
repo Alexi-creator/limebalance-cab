b43ac131447f5538fe40089251ee3fe95f3f1895
ilia <pavlov.il.creator@gmail.com>
2026-05-24 23:48:44 +0700

    fix styles and fonts

--- src/theme.ts
+++ src/theme.ts
@@ -4,9 +4,13 @@ export const theme = createTheme({
   primaryColor: "lime",
   defaultRadius: "md",
   fontFamily: "Inter, sans-serif",
+  fontFamilyMonospace: "Geist Mono, monospace",
   components: {
     Button: {
       defaultProps: { fw: 500 },
+    },
+    Paper: {
+      defaultProps: { withBorder: true },
     },
   },
 })
